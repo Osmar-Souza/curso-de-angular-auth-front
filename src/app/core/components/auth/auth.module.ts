a782121c8995157c9944610e403801d29a902cc6
Osmar-Souza <89655539+Osmar-Souza@users.noreply.github.com>
2023-02-28 13:12:48 -0300

    Updated

---
+++ src/app/core/components/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SignComponent } from './pages/sign/sign.component';
+
+
+@NgModule({
+  declarations: [
+    SignComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ]
+})
+export class AuthModule { }
---
+++ src/app/core/components/auth/pages/sign/sign.component.ts
@@ -0,0 +1,39 @@
+import { AuthService } from './../../../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign',
+  templateUrl: './sign.component.html',
+  styleUrls: ['./sign.component.scss']
+})
+export class SignComponent implements OnInit{
+
+  public formAuth: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  })
+
+  public msgError!: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService
+    ) {}
+
+  ngOnInit(): void {
+
+  }
+
+  public submitForm(){
+    if(this.formAuth.valid){
+      this.authService.sign({
+        email: this.formAuth.value.email,
+        password: this.formAuth.value.password,
+      }).subscribe({
+        next: (res) => res,
+        error: (e) => (this.msgError = e),
+      })
+    }
+  }
+
+}
